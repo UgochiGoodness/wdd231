5e14c82e8217987f40d4ecf8ca98bc3b3f04d6f4
UgochiGoodness <ugochigoodness08@gmail.com>
2026-01-25 04:03:43 +0100

    Week2 updates

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -55,3 +55,14 @@ function getMembershipLabel(level) {
     return "Member";
   }
 }
+
+const menuButton = document.querySelector("#menu");
+const nav = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  menuButton.classList.toggle("open");
+
+  const expanded = menuButton.getAttribute("aria-expanded") === "true";
+  menuButton.setAttribute("aria-expanded", !expanded);
+});
