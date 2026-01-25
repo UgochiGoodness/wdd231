316e65735c103c28f74777ef62e61d4e666da625
UgochiGoodness <ugochigoodness08@gmail.com>
2026-01-25 03:36:52 +0100

    week2

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,57 @@
+const membersContainer = document.querySelector("#members");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+async function getMembers() {
+  const response = await fetch("data/members.json");
+  const data = await response.json();
+  displayMembers(data.members);
+}
+
+function displayMembers(members) {
+  members.forEach(member => {
+    const card = document.createElement("section");
+
+   card.innerHTML = `
+  <img src="images/${member.image}" alt="${member.name}">
+  <h3>${member.name}</h3>
+
+  <span class="badge ${getMembershipLabel(member.membership).toLowerCase()}">
+    ${getMembershipLabel(member.membership)}
+  </span>
+
+  <p>${member.address}</p>
+  <p>${member.phone}</p>
+  <a href="${member.website}" target="_blank">Website</a>
+`;
+
+
+    membersContainer.appendChild(card);
+  });
+}
+
+gridButton.addEventListener("click", () => {
+  membersContainer.classList.add("grid");
+  membersContainer.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+  membersContainer.classList.add("list");
+  membersContainer.classList.remove("grid");
+});
+
+// Footer dates
+document.querySelector("#year").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+getMembers();
+
+function getMembershipLabel(level) {
+  if (level === 3) {
+    return "Gold";
+  } else if (level === 2) {
+    return "Silver";
+  } else {
+    return "Member";
+  }
+}
